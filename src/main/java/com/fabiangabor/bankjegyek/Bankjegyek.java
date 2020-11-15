8fdfb1403bc9d0566f4fc0ed70a3c113861fe7e5
Fábián Gábor <info@fabiangabor.com>
2020-11-15 18:45:06 +0100

    main init

--- src/main/java/com/fabiangabor/bankjegyek/Bankjegyek.java
+++ src/main/java/com/fabiangabor/bankjegyek/Bankjegyek.java
@@ -1,4 +1,34 @@
 package com.fabiangabor.bankjegyek;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Bankjegyek {
+    private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+    private JButton[][] squares = new JButton[5][5];
+    private JPanel bankjegyPanel;
+
+    Bankjegyek() {
+    }
+
+    public final JComponent getGui() {
+        return gui;
+    }
+
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            Bankjegyek bankjegyek = new Bankjegyek();
+
+            JFrame f = new JFrame("Bankjegyek");
+            f.add(bankjegyek.getGui());
+            f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            f.setLocationByPlatform(true);
+
+            f.pack();
+
+            f.setMinimumSize(f.getSize());
+            f.setVisible(true);
+        };
+        SwingUtilities.invokeLater(r);
+    }
 }
