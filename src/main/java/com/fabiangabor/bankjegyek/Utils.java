e36accf7ce77795ed9e3ecbce5c43a2ea53a3c34
Fábián Gábor <info@fabiangabor.com>
2020-12-04 23:52:08 +0100

    Sor- és oszlopösszegek frissítése

--- src/main/java/com/fabiangabor/bankjegyek/Utils.java
+++ src/main/java/com/fabiangabor/bankjegyek/Utils.java
@@ -17,6 +17,8 @@ public class Utils {
     private int[] editorSumRows;
     private int[] editorSumCols;
 
+    HashMap<String, Component> componentMap = new HashMap<>();
+
     public Utils() {
     }
 
@@ -37,14 +39,64 @@ public class Utils {
                 } catch (NumberFormatException numberFormatException) {
                     tmp[i][j] = 0;
                 }
-
             }
         }
         setEditorSquares(tmp);
     }
 
+    public int[] getEditorSumRows() {
+        return editorSumRows;
+    }
+
+    public void setEditorSumRows() {
+        this.editorSumRows = calculateEditorSumRows();
+    }
+
+    public int[] calculateEditorSumRows() {
+        int[] tmp = new int[editorSquares.length];
+
+        for (int i=0; i<editorSquares.length; i++) {
+            int[] count = new int[editorSquares.length+1];
+            for (int j=0; j<editorSquares.length; j++) {
+                if (count[editorSquares[i][j]] == 0) {
+                    tmp[i] += editorSquares[i][j];
+                }
+                count[editorSquares[i][j]]++;
+            }
+        }
+        return tmp;
+    }
+
+    public int[] getEditorSumCols() {
+        return editorSumCols;
+    }
+
+    public void setEditorSumCols() {
+        this.editorSumCols = calculateEditorSumCols();
+    }
+
+    public int[] calculateEditorSumCols() {
+        int[] tmp = new int[editorSquares.length];
+
+        for (int i=0; i<editorSquares.length; i++) {
+            int[] count = new int[editorSquares.length+1];
+            for (int j=0; j<editorSquares.length; j++) {
+                if (count[editorSquares[j][i]] == 0) {
+                    tmp[i] += editorSquares[j][i];
+                }
+                count[editorSquares[j][i]]++;
+            }
+        }
+        return tmp;
+    }
+
+
     public boolean collinear(Coordinates.Coord c1, Coordinates.Coord c2, Coordinates.Coord c3) {
         return (c1.y - c2.y) * (c1.x - c3.x) == (c1.y - c3.y) * (c1.x - c2.x);
+    }
+
+    public int[] count() {
+        return null;
     }
 
     public boolean checkEditor(JTextField[][] squares) {
@@ -73,8 +125,8 @@ public class Utils {
         }
 
         // ellenőrizzük, hogy minden táblán szerelő bankjegy értékéből 3 db van
-        for (int i=0; i<count.length; i++) {
-            if (!(count[i] == 0 || count[i] == 3)) {
+        for (int i : count) {
+            if (!(i == 0 || i == 3)) {
                 return false;
             }
         }
@@ -87,12 +139,55 @@ public class Utils {
                 if (!collinear(entry.getValue().get(0), entry.getValue().get(1), entry.getValue().get(2))) return false;
 
                 // így biztosan csak függőlegese vagy vízszintesen vannak egy vonalban
-                if (!((entry.getValue().get(0).x==entry.getValue().get(1).x) && (entry.getValue().get(0).x == entry.getValue().get(2).x) ||
-                        (entry.getValue().get(0).y==entry.getValue().get(1).y) && (entry.getValue().get(0).y == entry.getValue().get(2).y))) {
+                if (!((entry.getValue().get(0).x == entry.getValue().get(1).x) && (entry.getValue().get(0).x == entry.getValue().get(2).x) ||
+                        (entry.getValue().get(0).y == entry.getValue().get(1).y) && (entry.getValue().get(0).y == entry.getValue().get(2).y))) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    public void buildMap(JPanel mapEditor) {
+        System.out.println(editorSquares);
+        setEditorSumRows();
+        setEditorSumCols();
+
+        createComponentMap(mapEditor);
+        // frissítsük a GUI-ban a sorok összegét
+        for (int i=0; i<getEditorSumRows().length; i++) {
+            Component component = getComponentByName("sumRow"+i+"");
+            int index = Integer.parseInt(component.getName().replaceAll("[^0-9]", ""));
+
+            if (component instanceof JLabel) {
+                JLabel myLabel = (JLabel) component;
+                myLabel.setText(String.valueOf(editorSumRows[i]));
+            }
+        }
+
+        // frissítsük a GUI-ban az oszlopok összegét
+        for (int i=0; i<getEditorSumCols().length; i++) {
+            Component component = getComponentByName("sumCol"+i+"");
+            int index = Integer.parseInt(component.getName().replaceAll("[^0-9]", ""));
+
+            if (component instanceof JLabel) {
+                JLabel myLabel = (JLabel) component;
+                myLabel.setText(String.valueOf(editorSumCols[i]));
+            }
+        }
+    }
+
+    public void createComponentMap(JPanel frame) {
+        Component[] components = frame.getComponents();
+        for (int i=0; i < components.length; i++) {
+            componentMap.put(components[i].getName(), components[i]);
+        }
+    }
+
+    public Component getComponentByName(String name) {
+        if (componentMap.containsKey(name)) {
+            return (Component) componentMap.get(name);
+        }
+        else return null;
     }
 }
