a4957e4549df8552b4d2ea49dbc3f19c760d73c9
Fábián Gábor <info@fabiangabor.com>
2020-12-04 21:39:35 +0100

    Koordinatak elmentese es bankjegyek megszamolasa

---
+++ src/main/java/com/fabiangabor/bankjegyek/Coordinates.java
@@ -0,0 +1,15 @@
+package com.fabiangabor.bankjegyek;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Coordinates {
+
+    public static class Coord {
+        public int x,y;
+    }
+
+    static class Coords {
+        ArrayList<Coord> coords = new ArrayList<>();
+    }
+}
--- src/main/java/com/fabiangabor/bankjegyek/Utils.java
+++ src/main/java/com/fabiangabor/bankjegyek/Utils.java
@@ -2,9 +2,9 @@ package com.fabiangabor.bankjegyek;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class Utils {
-
     private static final Color[] colors = {
             Colors.pink,
             Colors.purple,
@@ -12,88 +12,69 @@ public class Utils {
             Colors.teal,
             Colors.green
     };
-    
-    public static Integer Sum(JTextField[][] squares, int index, boolean vertical) {
-        int sum = 0;
-        int num;
-        Integer[] countNum = {0,0,0,0,0};
-        String squareText;
 
-        for (int i=0; i<5; i++) {
-            if (vertical)
-                squareText = squares[i][index].getText();
-            else
-                squareText = squares[index][i].getText();
-            if (squareText.equals("")) squareText = "0"; // null kezeles
-            num = Integer.parseInt( squareText );
-            if (num > 0) { // [num - 1] kezeles
-                if (countNum[num - 1] == 0 || countNum[num - 1] == null) {
-                    countNum[num - 1]++;
-                    sum += num;
+    private int[][] editorSquares;
+    private int[] editorSumRows;
+    private int[] editorSumCols;
+
+    public Utils() {
+    }
+
+    public int[][] getEditorSquares() {
+        return editorSquares;
+    }
+
+    public void setEditorSquares(int[][] editorSquares) {
+        this.editorSquares = editorSquares;
+    }
+
+    public void convertJTextFieldToInt(JTextField[][] squares) {
+        int[][] tmp = new int[squares.length][squares.length];
+        for (int i=0; i<squares.length; i++) {
+            for (int j = 0; j < squares.length; j++) {
+                try {
+                    tmp[i][j] = Integer.parseInt(squares[i][j].getText());
+                } catch (NumberFormatException numberFormatException) {
+                    tmp[i][j] = 0;
+                }
+
+            }
+        }
+        setEditorSquares(tmp);
+    }
+
+    public boolean checkEditor(JTextField[][] squares) {
+        convertJTextFieldToInt(squares);
+
+        int size = squares.length;
+        int[] count = new int[size];
+
+        HashMap<Integer, ArrayList<Coordinates.Coord>> map = new HashMap<>();
+
+        for (int i=0; i<size; i++) {
+            for (int j = 0; j < size; j++) {
+                int value = this.editorSquares[i][j];
+
+                if (value > 0) {
+                    if (count[value-1] > 3) return false;
+                    count[value-1]++;
+                    Coordinates.Coord p = new Coordinates.Coord();
+                    p.x = i;
+                    p.y = j;
+
+                    map.putIfAbsent(value, new Coordinates.Coords().coords);
+                    map.get(value).add(p);
                 }
             }
         }
-        return sum;
-    }
 
-    public static void calculateRowColSum(JPanel jPanel, JTextField[][] squares, int[] rowSums, int[] colSums, JTextField textField, boolean editor) {
-        String id = (editor) ? "id" : "id-play";
-
-        int row = (editor) ? Integer.parseInt(textField.getClientProperty(id).toString())/10 : Integer.parseInt(textField.getClientProperty(id).toString())/10;
-        int rowSum = Sum(squares, row, false);
-        String getName = (editor) ? String.valueOf(row * 10) : String.valueOf(row * 100);
-        findLabelByName(jPanel, getName).setText(String.valueOf(rowSum));
-
-        int col = (editor) ? Integer.parseInt(textField.getClientProperty(id).toString())%10 : Integer.parseInt(textField.getClientProperty(id).toString())%10%10;
-        int colSum = Sum(squares, col, true);
-        getName = (editor) ? String.valueOf(col + 50) : String.valueOf((col + 50)*10);
-        findLabelByName(jPanel, getName).setText(String.valueOf(colSum));
-    }
-
-    public static JLabel findLabelByName(Container parent, String name) {
-        if (name != null) {
-            for (Component child : parent.getComponents()) {
-                if (child instanceof JLabel) {
-                    JLabel label = (JLabel)child;
-                    if (name.equals(label.getName())) {
-                        return label;
-                    }
-                }
+        // ellenőrizzük, hogy minden táblán szerelő bankjegy értékéből 3 db van
+        for (int i=0; i<count.length; i++) {
+            if (!(count[i] == 0 || count[i] == 3)) {
+                return false;
             }
         }
-        return null;
-    }
 
-    public static void check(JTextField jTextField, int[] countBankjegyek, JTextField[][] squares) {
-        int inputNum;
-        try {
-            inputNum = Integer.parseInt(jTextField.getText());
-
-            if (inputNum > 0 && inputNum < 6)
-                jTextField.setBackground(colors[inputNum-1]);
-        }
-        catch (NumberFormatException numberFormatException) {
-            inputNum = 0;
-            // jTextField koordinataja kell, ott kell csokkenteni 1-gyel a szamjegy mennyiseget a count-ban
-
-            int row = Integer.parseInt(jTextField.getClientProperty("id").toString())/10;
-            int col = Integer.parseInt(jTextField.getClientProperty("id").toString())%10;
-
-            System.out.println("[" + row + "][" + col + "] = " + squares[row][col].getText() + jTextField.getText());
-        }
-
-        System.out.println(inputNum);
-    }
-
-    public static void print(JTextField[][] squares) {
-        for (JTextField[] square : squares) {
-            for (JTextField jTextField : square) {
-                String text = jTextField.getText();
-                System.out.print((text.length() > 0) ? text : " ");
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-        System.out.println("-----------------------------------");
+        return true;
     }
 }
