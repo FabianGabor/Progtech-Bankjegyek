bfcbf9e35f1d21df204cce7a66d50c36a2e26bd1
Fábián Gábor <info@fabiangabor.com>
2020-12-05 16:34:34 +0100

    input parameter for main()

--- src/main/java/com/fabiangabor/bankjegyek/Bankjegyek.java
+++ src/main/java/com/fabiangabor/bankjegyek/Bankjegyek.java
@@ -2,7 +2,9 @@ package com.fabiangabor.bankjegyek;
 
 public class Bankjegyek {
     public static void main(String[] args) {
-        GUI gui = new GUI(5);
-        gui.getGui();
+        int size = 5;
+        if (args.length > 0)
+            size = Integer.parseInt(args[0]);
+        new GUI(size);
     }
 }
