d7b3713f52f3b0d538995d30a0300ff74e6548b9
Fábián Gábor <info@fabiangabor.com>
2020-12-08 21:21:32 +0100

    Test

---
+++ src/main/java/com/fabiangabor/bankjegyek/UtilsTest.java
@@ -0,0 +1,59 @@
+package com.fabiangabor.bankjegyek;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UtilsTest {
+
+    @Test
+    void convertJTextFieldToInt() {
+    }
+
+    @Test
+    void calculateSumRows() {
+    }
+
+    @Test
+    void calculateSumCols() {
+    }
+
+    @Test
+    void checkCount() {
+    }
+
+    @Test
+    void collinear() {
+    }
+
+    @Test
+    void checkCollinear() {
+    }
+
+    @Test
+    void buildDataMap() {
+    }
+
+    @Test
+    void checkEditor() {
+    }
+
+    @Test
+    void checkRowColSums() {
+    }
+
+    @Test
+    void checkPlay() {
+    }
+
+    @Test
+    void calculateSums() {
+    }
+
+    @Test
+    void createComponentMap() {
+    }
+
+    @Test
+    void getComponentByName() {
+    }
+}
