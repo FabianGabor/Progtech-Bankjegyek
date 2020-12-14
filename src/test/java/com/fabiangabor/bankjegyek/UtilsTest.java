e40bef4371ca0935d4835d8164927b1ddadc15e6
Fábián Gábor <info@fabiangabor.com>
2020-12-14 14:32:07 +0100

    test set Order()

--- src/test/java/com/fabiangabor/bankjegyek/UtilsTest.java
+++ src/test/java/com/fabiangabor/bankjegyek/UtilsTest.java
@@ -1,16 +1,16 @@
 package com.fabiangabor.bankjegyek;
 
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.stream.IntStream;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class UtilsTest {
     Utils utils = new Utils();
     static JTextField[][] jTextFieldArray;
@@ -23,11 +23,12 @@ public class UtilsTest {
     }
     DataContainer dataContainer = new DataContainer();
 
-    static ArrayList<DataContainer> dataArray = new ArrayList<DataContainer>();
+    static ArrayList<DataContainer> dataArray = new ArrayList<>();
 
     private static IntStream range() {
         return IntStream.range(1, 20);
     }
+
 
     @Order(1)
     @ParameterizedTest
@@ -49,45 +50,48 @@ public class UtilsTest {
         dataContainer.jTextFieldArray = jTextFieldArray;
         dataArray.add(dataContainer);
 
-        convertJTextFieldToInt(n);
-        calculateSumRows(n);
-        calculateSumCols(n);
+        //convertJTextFieldToInt(n);
+        //calculateSumRows(n);
+        //calculateSumCols(n);
     }
 
-    //@Order(2)
-    //@ParameterizedTest
-    //@MethodSource("range")
+    @Order(2)
+    @ParameterizedTest
+    @MethodSource("range")
     public void convertJTextFieldToInt(int n) {
         Assertions.assertArrayEquals(dataArray.get(n-1).intArray, utils.convertJTextFieldToInt(dataArray.get(n-1).jTextFieldArray));
     }
 
-    //@Order(3)
-    //@ParameterizedTest
-    //@MethodSource("range")
+    @Order(3)
+    @ParameterizedTest
+    @MethodSource("range")
     public void calculateSumRows(int n) {
         int[] sums = new int[n];
 
         for (int i=0; i<n; i++) {
             int[] count = new int[dataArray.get(n-1).intArray.length+1];
             for (int j=0; j<n; j++) {
-                if (count[dataArray.get(n - 1).intArray[i][j]] == 0) {
-                    sums[i] += dataArray.get(n - 1).intArray[i][j];
-                    count[dataArray.get(n - 1).intArray[i][j]]++;
+                if (count[dataArray.get(n-1).intArray[i][j]] == 0) {
+                    sums[i] += dataArray.get(n-1).intArray[i][j];
+                    count[dataArray.get(n-1).intArray[i][j]]++;
                 }
             }
         }
         Assertions.assertArrayEquals(sums, utils.calculateSumRows(dataArray.get(n-1).intArray));
     }
 
+    @Order(3)
+    @ParameterizedTest
+    @MethodSource("range")
     public void calculateSumCols(int n) {
         int[] sums = new int[n];
 
         for (int j=0; j<n; j++) {
             int[] count = new int[dataArray.get(n-1).intArray.length+1];
             for (int i=0; i<n; i++) {
-                if (count[dataArray.get(n - 1).intArray[i][j]] == 0) {
-                    sums[j] += dataArray.get(n - 1).intArray[i][j];
-                    count[dataArray.get(n - 1).intArray[i][j]]++;
+                if (count[dataArray.get(n-1).intArray[i][j]] == 0) {
+                    sums[j] += dataArray.get(n-1).intArray[i][j];
+                    count[dataArray.get(n-1).intArray[i][j]]++;
                 }
             }
         }
